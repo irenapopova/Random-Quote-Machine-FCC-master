f63331b7bb8080498caeb4a31836986473d87cb1
irenapopova <irenejpopova6@gmail.com>
2019-06-12 18:59:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Random-Quote-Machine-FCC
+
+
+
+The random quote machine is a web application that generate a random quote and allows you to tweet it.  
---
+++ js/quotes.js
@@ -0,0 +1,64 @@
+
+/*
+
+ Code by Irena
+
+*/
+
+$(document).ready(function () {
+    var author;
+    var quote;
+    var color;
+
+     /* A set of colors */
+    var colors = ['#0074D9', '#7FDBFF', '#39CCCC', '#FFDC00', '#01FF70', '#2ECC40',
+                  '#3D9970', '#FF851B', '#FF4136', '#85144b', '#F012BE', '#B10DC9'];
+
+    $("#getQuotes").on("click", function () {
+
+        var index = Math.floor(Math.random() * (colors.length));
+        color = colors[index];
+        $('#failedTweet').addClass('invisible');
+        $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+            function (json) {
+                quote = json.quoteText;
+                author = json.quoteAuthor;
+
+
+                var html = "<h1> &ldquo; " + quote + "  &rdquo;</h1> <br>";
+                html += "<h3> --" + author + "</h3>";
+                $("#display").html(html);
+                $("body").css("background-color", color);
+
+
+            });
+    });
+
+    $("#shareQuote").on("click", function () {
+        //$('span p').remove();
+
+        var textToTweet = "#TodayQuote  " + quote + " --" + author;
+        console.log(textToTweet);
+        if (textToTweet.length > 240) {
+            $("#failedTweet").removeClass("invisible");
+
+        } else {
+            var twtLink = "https://twitter.com/home?status=" + encodeURIComponent(textToTweet);
+            window.open(twtLink, "_blank", "width=800, height=300");
+        }
+    });
+
+
+     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+            function (json) {
+                quote = json.quoteText;
+                author = json.quoteAuthor;
+
+
+                var html = "<h1> &ldquo; " + quote + "  &rdquo;</h1> <br>";
+                html += "<h3> --" + author + "</h3>";
+                $("#display").html(html);
+
+
+            });
+});
